40108a79b1194a1f5b73bc49ecf5a3a5fc89c1b5
Gabriele Alleruzzo <alleruzzo.gabriele.96@gmail.com>
2025-03-19 16:48:33 +0100

    idk

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let getBooks = function () {
-  const urlBooks = "https://striveschool-api.herokuapp.com/bookspalle";
+  const urlBooks = "https://striveschool-api.herokuapp.com/books";
 
   fetch(urlBooks)
     .then((data) => {
       if (data.ok) {
-        console.log("bho libri", data);
+        console.log("good ending", data);
+        return data.json;
       } else {
-        throw new error("boja-de");
+        throw new error("errore 0");
       }
     })
+    .then((dati) => {
+      console.log("libriii", dati);
+    })
     .catch((err) => {
-      console.log("no libri");
+      console.log("bad ending");
     });
 };
 
